bb5794b77a5fb6ff4e8ae7729e6b6726b90217f0
Nirajan <nirajanshrestha672@gmail.com>
2024-09-15 23:39:39 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,27 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
+// Middleware
+const allowedOrigins = [
+  "https://traktiva.onrender.com/", // frontend Render URL
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        // Allow requests with no origin (e.g., Postman)
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow headers
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  })
+);
+
 //Session setup
 app.use(
   session({
@@ -19,6 +40,7 @@ app.use(
     resave: false, //resave: forces a session to be stored in a session store (ex: database)
     saveUninitialized: false, //saveUninitialaized: forces a session that is unInitialized to be savd in session store, a session is UI when it is new but not modiefied
     cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // Cookie expiry time
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Secure in production
       sameSite: "none", // For cross-site requests
@@ -29,18 +51,6 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Middleware
-const allowedOrigins = [
-  "https://traktiva.onrender.com/", // frontend Render URL
-];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
